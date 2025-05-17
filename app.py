df7f01314fc8761b660f327f735bdc98b2be3b88
D4AX7 <jenaashutosh17@gmail.com>
2025-05-17 14:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ with st.sidebar:
 
 llm_engine=ChatOllama(
     model=selected_model,
-    base_url="http://localhost:11434",
+    base_url="http://host.docker.internal:11434",
 
     temperature=0.3
 
